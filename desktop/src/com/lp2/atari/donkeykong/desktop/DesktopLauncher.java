5d09740128778463855bda13f23799f84adee19f
Thais <alvesthais406@gmail.com>
2021-02-02 10:20:36 -0300

    teste

---
+++ desktop/src/com/lp2/atari/donkeykong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lp2.atari.donkeykong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lp2.atari.donkeykong.DonkeyKong;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DonkeyKong(), config);
+	}
+}
